a16681fa6ee327787323aa0b89f8874b4035f75f
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2025-11-20 11:46:38 +0100

    Remove pacman key initialization from script
    Removed pacman key initialization steps from the script.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,12 +4,6 @@ set -eu
 
 ARCH=$(uname -m)
 
-
-echo "Initialisation de la clé pacman..."
-echo "---------------------------------------------------------------"
-pacman-key --init
-pacman-key --populate
-
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
